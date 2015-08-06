b4205376068cb2e797e55e7aa6ed18706d51bcc7
Clay Walker <clayzermk1@gmail.com>
2015-08-05 19:04:24 -0700

    Check for process variable in case the file isn't parsed.

--- numbro.js
+++ numbro.js
@@ -962,7 +962,7 @@
     if (hasModule) {
         module.exports = numbro;
 
-        if (process.title === 'node') {
+        if (process !== undefined && process.title === 'node') {
             // Load all languages
             var fs = require('fs');
             var path = require('path');
