7edad6c5025d5653b3de814b11538eeb67facfaa
Priit <priit.hansen@gmail.com>
2016-10-03 00:10:35 +0300

    Create lt-LT.js
    Lithuanian support

---
+++ languages/lt-LT.js
@@ -0,0 +1,49 @@
+/*!
+ * numbro.js language configuration
+ * language : Lithuanian
+ * locale: Lithuania
+ * author : Agnė
+ */
+(function () {
+    'use strict';
+
+    var language = {
+        langLocaleCode: 'lt-LT',
+        cultureCode: 'lt-LT',
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: ' tūkst.',
+            million: ' mln.',
+            billion: ' mlrd.',
+            trillion: ' trln.'
+        },
+        ordinal: function () {
+            return '.';
+        },
+        currency: {
+            symbol: '€',
+            position: 'postfix',
+            code: 'USD'
+        },
+        defaults: {
+            currencyFormat: ',4 a'
+        },
+        formats: {
+            fourDigits: '4 a',
+            fullWithTwoDecimals: ',0.00 $',
+            fullWithTwoDecimalsNoCurrency: ',0.00',
+            fullWithNoDecimals: ',0 $'
+        }
+    };
+    // CommonJS
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && window.numbro && window.numbro.culture) {
+        window.numbro.culture(language.cultureCode, language);
+    }
+}.call(typeof window === 'undefined' ? this : window));
