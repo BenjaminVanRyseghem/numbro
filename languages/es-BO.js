3ac60c8f9174eac97fdf63c45deb5edee1d29e4c
Jo Colina <jo.colina@weezevent.com>
2018-10-16 08:00:43 +0200

    Added Bolivian Boliviano as a currency

---
+++ languages/es-BO.js
@@ -0,0 +1,58 @@
+/*!
+ * numbro.js language configuration
+ * language : Spanish
+ * locale: Mexico
+ * author : Joe Bordes : https://github.com/joebordes
+ */
+
+module.exports = {
+    languageTag: "es-BO",
+    delimiters: {
+        thousands: ",",
+        decimal: "."
+    },
+    abbreviations: {
+        thousand: "k",
+        million: "mm",
+        billion: "b",
+        trillion: "t"
+    },
+    ordinal: function(number) {
+        let b = number % 10;
+        return (b === 1 || b === 3) ? "er" : (b === 2) ? "do" : (b === 7 || b === 0) ? "mo" : (b === 8) ? "vo" : (b === 9) ? "no" : "to";
+    },
+    currency: {
+        symbol: "Bs",
+        position: "prefix",
+        code: "BOB"
+    },
+    currencyFormat: {
+        thousandSeparated: true,
+        totalLength: 4,
+        spaceSeparated: true,
+        average: true
+    },
+    formats: {
+        fourDigits: {
+            totalLength: 4,
+            spaceSeparated: true,
+            average: true
+        },
+        fullWithTwoDecimals: {
+            output: "currency",
+            mantissa: 2,
+            spaceSeparated: true,
+            thousandSeparated: true
+        },
+        fullWithTwoDecimalsNoCurrency: {
+            mantissa: 2,
+            thousandSeparated: true
+        },
+        fullWithNoDecimals: {
+            output: "currency",
+            spaceSeparated: true,
+            thousandSeparated: true,
+            mantissa: 0
+        }
+    }
+};
