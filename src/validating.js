a4ac8c36692e9a27a588ca4b7d4af4e9c7940935
bitGatito <usmanbinmujeeb@gmail.com>
2025-09-19 19:45:12 +0800

    feat: BCP47 ABNF syntax for Language Tags regex

--- src/validating.js
+++ src/validating.js
@@ -22,8 +22,8 @@
 
 let unformatter = require("./unformatting");
 
-// Simplified regexp supporting only `language`, `script`, and `region`
-const bcp47RegExp = /^[a-z]{2,3}(-[a-zA-Z]{4})?(-([A-Z]{2}|[0-9]{3}))?$/;
+// Regexp supporting the BCP47 ABNF syntax 
+const bcp47RegExp = /^((?<grandfathered>(en-GB-oed|i-ami|i-bnn|i-default|i-enochian|i-hak|i-klingon|i-lux|i-mingo|i-navajo|i-pwn|i-tao|i-tay|i-tsu|sgn-BE-FR|sgn-BE-NL|sgn-CH-DE)|(art-lojban|cel-gaulish|no-bok|no-nyn|zh-guoyu|zh-hakka|zh-min|zh-min-nan|zh-xiang))|((?<language>([A-Za-z]{2,3}(-(?<extlang>[A-Za-z]{3}(-[A-Za-z]{3}){0,2}))?)|[A-Za-z]{4}|[A-Za-z]{5,8})(-(?<script>[A-Za-z]{4}))?(-(?<region>[A-Za-z]{2}|[0-9]{3}))?(-(?<variant>[A-Za-z0-9]{5,8}|[0-9][A-Za-z0-9]{3}))*(-(?<extension>[0-9A-WY-Za-wy-z](-[A-Za-z0-9]{2,8})+))*(-(?<privateUse1>x(-[A-Za-z0-9]{1,8})+))?)|(?<privateUse2>x(-[A-Za-z0-9]{1,8})+))$/;
 
 const validOutputValues = [
     "currency",
